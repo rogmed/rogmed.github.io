fb7549b83c91511bee027cb602c62f5144f82eaf
rogmed <rogeliomdn@gmail.com>
2025-06-01 23:11:29 +0200

    Data mastery

---
+++ js/data_mastery.js
@@ -0,0 +1,89 @@
+data_mastery = [
+    {
+        title: "Debilitar",
+        icon: "sword-break",
+        subtitle: "Impones desventaja en su próximo ataque",
+        description: "",
+        reference: "PHB, pg. 214.",
+        bullets: [
+            "Si aciertas a una criatura con esta arma, tendrá desventaja en su próxima tirada de ataque antes del principio de tu siguiente turno"
+        ]
+    },
+    {
+        title: "Derribar",
+        icon: "crawl",
+        subtitle: "Derribas a una criatura",
+        description: "",
+        reference: "PHB, pg. 214.",
+        bullets: [
+            "Si aciertas con un ataque con esta arma, tirada de salvación de Constitución (CD 8 + mod. de característica del ataque + bon. competencia), o la criatua queda derribada."
+        ]
+    },
+    {
+        title: "Empujar",
+        icon: "hammer-drop",
+        subtitle: "Empujas a una criatura 10 pies",
+        description: "",
+        reference: "PHB, pg. 214.",
+        bullets: [
+            "Si aciertas con un ataque con esta arma, empujas a una criatura hasta 10 pies en linea recta si es Grande o mas pequeña."
+        ]
+    },
+    {
+        title: "Hender",
+        icon: "axe-swing",
+        subtitle: "Ataque extra contra criatura adyacente",
+        description: "",
+        reference: "PHB, pg. 214.",
+        bullets: [
+            "Si aciertas a una criatura con una tirada de ataque cuerpo a cuerpo, puedes hacer ataque contra otra criatura",
+            "Debe de estar a 5 pies o menos de la primera y a tu alcance",
+            "No sumas el modificador por característica al segundo ataque, a menos que sea negativo",
+            "Solo puedes  hacer este ataque extra una vez por turno"
+        ]
+    },
+    {
+        title: "Mellar",
+        icon: "daggers",
+        subtitle: "Ataque extra con arma ligera",
+        description: "",
+        reference: "PHB, pg. 214.",
+        bullets: [
+            "Cuando hagas el ataque extra de la propiedad 'ligera', puedes hacerlo como parte de la acción de atacar en vez de como acción adicional.",
+            "Solo puedes hacer este ataque extra una vez por turno."
+        ]
+    },
+    {
+        title: "Molestar",
+        icon: "cracked-shield",
+        subtitle: "Tienes ventaja en tu próximo ataque",
+        description: "",
+        reference: "PHB, pg. 214.",
+        bullets: [
+            "Si aciertas a una criatura con esta arma y causas daño, tendrás ventaja en tu próximo ataque contra esa criatura antes del final de tu siguiente turno."
+        ]
+    },
+    {
+        title: "Ralentizar",
+        icon: "foot-trip",
+        subtitle: "Reduces velocidad en 10 pies",
+        description: "",
+        reference: "PHB, pg. 214.",
+        bullets: [
+            "Reduce velocidad en 10 pies hasta el principio de tu siguiente turno.",
+            "No se puede reducir mas de 10 pies aunque reciba varios ataques"
+        ]
+    },
+    {
+        title: "Rozar",
+        icon: "zeus-sword",
+        subtitle: "Causas daño aunque falles",
+        description: "",
+        reference: "PHB, pg. 214.",
+        bullets: [
+            "Si tu tirada de ataque no acierta contra una criatura, causas daño igual al modificador por característica del ataque",
+            "El daño es del mismo tipo que el del arma",
+            "Solo se puede incrementar si aumentas el modificador por característica"
+        ]
+    },
+]
