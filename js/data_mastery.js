fa54404359a5511700cc44e0ff3d880a17f7d512
Rogelio Medina Medina <rogelio.medina@unit4.com>
2025-06-02 11:32:18 +0200

    Armas incluidas en cada maestría

--- js/data_mastery.js
+++ js/data_mastery.js
@@ -6,7 +6,18 @@ data_mastery = [
         description: "",
         reference: "PHB, pg. 214.",
         bullets: [
-            "Si aciertas a una criatura con esta arma, tendrá desventaja en su próxima tirada de ataque antes del principio de tu siguiente turno"
+            "Si aciertas a una criatura con esta arma, tendrá desventaja en su próxima tirada de ataque antes del principio de tu siguiente turno",
+            "Armas que tienen esta propiedad:" +
+            "<br><br>" +
+            "Sencillas:<ul>" +
+            "<li>Lanza</li>" +
+            "<li>Maza</li>" +
+            "</ul>Marciales:<ul>" +
+            "<li>Espada larga</li>" +
+            "<li>Lucero del alba</li>" +
+            "<li>Mangual</li>" +
+            "<li>Pico de guerra</li>" +
+            "</ul>"
         ]
     },
     {
@@ -16,7 +27,17 @@ data_mastery = [
         description: "",
         reference: "PHB, pg. 214.",
         bullets: [
-            "Si aciertas con un ataque con esta arma, tirada de salvación de Constitución (CD 8 + mod. de característica del ataque + bon. competencia), o la criatua queda derribada."
+            "Si aciertas con un ataque con esta arma, tirada de salvación de Constitución (CD 8 + mod. de característica del ataque + bon. competencia), o la criatua queda derribada.",
+            "Armas que tienen esta propiedad:" +
+            "<br><br>" +
+            "Sencillas:<ul>" +
+            "<li>Bastón</li>" +
+            "</ul>Marciales:<ul>" +
+            "<li>Hacha de guerra</li>" +
+            "<li>Lanza de caballería</li>" +
+            "<li>Maza a dos manos</li>" +
+            "<li>Tridente</li>" +
+            "</ul>"
         ]
     },
     {
@@ -26,7 +47,16 @@ data_mastery = [
         description: "",
         reference: "PHB, pg. 214.",
         bullets: [
-            "Si aciertas con un ataque con esta arma, empujas a una criatura hasta 10 pies en linea recta si es Grande o mas pequeña."
+            "Si aciertas con un ataque con esta arma, empujas a una criatura hasta 10 pies en linea recta si es Grande o mas pequeña.",
+            "Armas que tienen esta propiedad:" +
+            "<br><br>" +
+            "Sencillas:<ul>" +
+            "<li>Garrote grande</li>" +
+            "</ul>Marciales:<ul>" +
+            "<li>Martillo de guerra</li>" +
+            "<li>Pica</li>" +
+            "<li>Ballesta pesada</li>" +
+            "</ul>"
         ]
     },
     {
@@ -39,7 +69,13 @@ data_mastery = [
             "Si aciertas a una criatura con una tirada de ataque cuerpo a cuerpo, puedes hacer ataque contra otra criatura",
             "Debe de estar a 5 pies o menos de la primera y a tu alcance",
             "No sumas el modificador por característica al segundo ataque, a menos que sea negativo",
-            "Solo puedes  hacer este ataque extra una vez por turno"
+            "Solo puedes  hacer este ataque extra una vez por turno",
+            "Armas que tienen esta propiedad:" +
+            "<br><br>" +
+            "Marciales:<ul>" +
+            "<li>Alabarda</li>" +
+            "<li>Hacha a dos manos</li>" +
+            "</ul>"
         ]
     },
     {
@@ -50,7 +86,16 @@ data_mastery = [
         reference: "PHB, pg. 214.",
         bullets: [
             "Cuando hagas el ataque extra de la propiedad 'ligera', puedes hacerlo como parte de la acción de atacar en vez de como acción adicional.",
-            "Solo puedes hacer este ataque extra una vez por turno."
+            "Solo puedes hacer este ataque extra una vez por turno.",
+            "Armas que tienen esta propiedad:" +
+            "<br><br>" +
+            "Sencillas:<ul>" +
+            "<li>Daga</li>" +
+            "<li>Hoz</li>" +
+            "<li>Martillo ligero</li>" +
+            "</ul>Marciales:<ul>" +
+            "<li>Cimitarra</li>" +
+            "</ul>"
         ]
     },
     {
@@ -60,7 +105,20 @@ data_mastery = [
         description: "",
         reference: "PHB, pg. 214.",
         bullets: [
-            "Si aciertas a una criatura con esta arma y causas daño, tendrás ventaja en tu próximo ataque contra esa criatura antes del final de tu siguiente turno."
+            "Si aciertas a una criatura con esta arma y causas daño, tendrás ventaja en tu próximo ataque contra esa criatura antes del final de tu siguiente turno.",
+            "Armas que tienen esta propiedad:" +
+            "<br><br>" +
+            "Sencillas:<ul>" +
+            "<li>Hacha de mano</li>" +
+            "<li>Arco corto</li>" +
+            "<li>Dardo</li>" +
+            "</ul>Marciales:<ul>" +
+            "<li>Espada corta</li>" +
+            "<li>Estoque</li>" +
+            "<li>Ballesta de mano</li>" +
+            "<li>Cerbatana</li>" +
+            "<li>Pistola</li>" +
+            "</ul>"
         ]
     },
     {
@@ -71,7 +129,19 @@ data_mastery = [
         reference: "PHB, pg. 214.",
         bullets: [
             "Reduce velocidad en 10 pies hasta el principio de tu siguiente turno.",
-            "No se puede reducir mas de 10 pies aunque reciba varios ataques"
+            "No se puede reducir mas de 10 pies aunque reciba varios ataques",
+            "Armas que tienen esta propiedad:" +
+            "<br><br>" +
+            "Sencillas:<ul>" +
+            "<li>Garrote</li>" +
+            "<li>Jabalina</li>" +
+            "<li>Ballesta ligera</li>" +
+            "<li>Honda</li>" +
+            "</ul>Marciales:<ul>" +
+            "<li>Látigo</li>" +
+            "<li>Arco largo</li>" +
+            "<li>Mosquete</li>" +
+            "</ul>"
         ]
     },
     {
@@ -83,7 +153,13 @@ data_mastery = [
         bullets: [
             "Si tu tirada de ataque no acierta contra una criatura, causas daño igual al modificador por característica del ataque",
             "El daño es del mismo tipo que el del arma",
-            "Solo se puede incrementar si aumentas el modificador por característica"
+            "Solo se puede incrementar si aumentas el modificador por característica",
+            "Armas que tienen esta propiedad:" +
+            "<br><br>" +
+            "Marciales:<ul>" +
+            "<li>Espadón</li>" +
+            "<li>Guja</li>" +
+            "</ul>"
         ]
     },
 ]
